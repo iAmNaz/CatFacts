ecd7b606698b80aa5165b8b21a8dc3b4428aee39
Naz Mariano <iamnazdelacruz@gmail.com>
2025-01-09 23:31:28 +0800

    Initial Commit

---
+++ CatFacts/CatFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatFactsApp.swift
+//  CatFacts
+//
+//  Created by Naz Mariano on 1/9/25.
+//
+
+import SwiftUI
+
+@main
+struct CatFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
